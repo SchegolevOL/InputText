1ed73551369fcfc8eee5022a15b16375f32f1092
Oleg Schegol <olschegolev@yahoo.com>
2023-02-18 18:57:56 +0500

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+let button = document.querySelector("#buttonFormatText");
+let text = document.querySelector("#text");
+
+
+button.addEventListener("click", (event) => {
+  let form = document.querySelector("form");
+
+  document.querySelector("#Bold").checked
+    ? text.style.setProperty("font-weight", "bold")
+    : text.style.setProperty("font-weight", "normal");
+
+  document.querySelector("#Underline").checked
+    ? text.style.setProperty("text-decoration", "underline")
+    : text.style.setProperty("text-decoration", "none");
+
+  document.querySelector("#Italics").checked
+    ? text.style.setProperty("font-style", "italic")
+    : text.style.setProperty("font-style", "normal");
+
+  var data = new FormData(form);
+  var output = "";
+  for (const entry of data) {
+    output = entry[1];
+  }
+  console.log(output);
+  switch (output) {
+    case "Left":      
+      text.style.setProperty("text-align", "left");
+      break;
+    case "Right":
+      text.style.setProperty("text-align", "right");
+      break;
+    case "Justify":
+      text.style.setProperty("text-align", "justify");
+      break;
+  }
+  console.log(text.style);
+ 
+});
